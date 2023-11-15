95b80067c8bd8cf16ec223fc2edc0c3c41658597
Limsukhyun <as17269@naver.com>
2023-11-15 15:22:17 +0900

    feat: vue

--- src/components/ShowVideoComp.vue
+++ src/components/ShowVideoComp.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="video-list">
-    <div v-for="video in videos" :key="video.id" class="video-item">
-      <video :src="video.path" controls></video>
+  <div class="video-list" :style="{ alignItems: videoListAlignItems }">
+    <div v-for="video in videos" :key="video.id" class="video-item" :data-video-path="video.path" ref="videoItem">
+      <video :src="video.path" controls muted ref="videoElement" @play="handleVideoPlay"></video>
       <h3>{{ video.title }}</h3>
       <p>{{ video.description }}</p>
     </div>
@@ -13,36 +13,81 @@ export default {
   data() {
     return {
       videos: [],
+      observer: null,
+      autoPlayVideoIndex: -1,
+      videoListAlignItems: 'center', // 초기값 설정
     };
   },
   mounted() {
-    // 컴포넌트가 마운트되면 비디오 데이터를 가져옴
     this.fetchVideos();
   },
   methods: {
     async fetchVideos() {
       try {
-        // 비디오 데이터 가져오는 API 호출
         const response = await fetch('http://localhost:7001/api/videos');
         const data = await response.json();
-        // 가져온 데이터를 컴포넌트의 데이터에 할당
-        this.videos = data.map(video => {
-          console.log(video);
-          return {
-            id: video.video_no,
-            title: video.title,
-            path: video.video_no,
-            // description: video.description,       부가적인 설명인듯? 내용?
-            // filePath: `data:${video.contentType};base64,${video.base64Data}`,
-            // path: video.path,
-            // path: '1699953924404.mp4',
-            // path: video.video_no,
-            // path: process.env.BASE_URL + video.path,
-          };
-        });
+        this.videos = data.map(video => ({
+          id: video.video_no,
+          title: video.title,
+          path: video.video_no,
+        }));
+
+        this.$nextTick(() => this.initIntersectionObserver());
       } catch (error) {
         console.error('Error fetching videos:', error);
       }
+    },
+    initIntersectionObserver() {
+      this.observer = new IntersectionObserver(this.handleIntersection, {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
+      });
+
+      if (this.$refs.videoItem) {
+        this.$refs.videoItem.forEach(item => this.observer.observe(item));
+      }
+    },
+    handleIntersection(entries) {
+      entries.forEach(entry => {
+        const videoElement = entry.target.querySelector('video');
+
+        if (entry.isIntersecting) {
+          videoElement.play();
+
+          if (this.autoPlayVideoIndex !== -1) {
+            this.$refs.videoItem[this.autoPlayVideoIndex].classList.remove('focused');
+          }
+
+          entry.target.classList.add('focused');
+          this.autoPlayVideoIndex = this.videos.findIndex(video => video.path === entry.target.dataset.videoPath);
+
+          // 왼쪽 정렬로 변경
+          this.videoListAlignItems = 'flex-start';
+
+          // 전체 바디에 배경색 적용
+          document.body.style.background = 'black';
+        } else {
+          videoElement.pause();
+          videoElement.currentTime = 0;
+
+          entry.target.classList.remove('focused');
+          document.body.style.background = 'none'; // 영상이 보이지 않을 때 배경 제거
+
+          // 다시 가운데 정렬로 변경
+          this.videoListAlignItems = 'center';
+        }
+      });
+    },
+    handleVideoPlay(event) {
+      this.$refs.videoItem.forEach(item => {
+        const videoElement = item.querySelector('video');
+        if (videoElement !== event.target) {
+          videoElement.pause();
+          videoElement.currentTime = 0;
+          item.classList.remove('focused');
+        }
+      });
     },
   },
 };
@@ -52,16 +97,23 @@ export default {
   .video-list {
     display: flex;
     flex-direction: column;
-    align-items: center;
   }
 
   .video-item {
-    width: 500px;
-    margin: 40px;
+    width: 800px;
+    height: 600px;
+    margin: 150px;
+    display: flex;
+    flex-direction: column;
+    align-items: center; /* 중앙 정렬로 초기화 */
+  }
+
+  .focused {
+    border: 2px solid black; /* 포커스 시 보더 추가 */
   }
 
   video {
     width: 100%;
-    height: auto;
+    height: 100%;
   }
 </style>
