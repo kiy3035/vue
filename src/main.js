ddbbc69de637e95df9716372d954e7871903fcde
김인영 <116708377+kiy3035@users.noreply.github.com>
2023-11-20 17:59:42 +0900

    feat: vue

--- src/main.js
+++ src/main.js
@@ -6,15 +6,19 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { faVideo } from '@fortawesome/free-solid-svg-icons';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faComment } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faFileLines } from '@fortawesome/free-solid-svg-icons';
+
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import axios from 'axios';
 
 
-library.add(faPlus);
-library.add(faTimes);
-library.add(faVideo);
-library.add(faCheck);
+library.add(faPlus, faTimes, faVideo, faCheck, faPlay, faComment, faThumbsUp, faCalendar, faUser, faFileLines);
 
 const app = createApp(App);
 
