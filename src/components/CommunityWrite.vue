51f315f4c1ad2cddc54f6a7f1593ecc25f1b9e8b
Limsukhyun <as17269@naver.com>
2023-11-21 09:50:34 +0900

    feat: vue

--- src/components/CommunityWrite.vue
+++ src/components/CommunityWrite.vue
@@ -1,228 +1,202 @@
 <template>
-  <div class="materialContainer">
+    <div class="materialContainer ui">
+  
+      <div class="box">
+  
+        <!-- 닫기 아이콘 -->
+        <button class="close-button" @click="closeForm">
+          <font-awesome-icon :icon="['fas', 'times']" />
+        </button>
+        <div class="title">글쓰기</div>
+  
+      <!-- Form -->
+      <form method="post" id="videoForm" @submit.prevent="submitForm" enctype="multipart/form-data">
+        <!-- 닉네임 -->
+        <div class="input" :class="{ 'focused': inputFocused === 'nickname', 'blured' : inputBlured === 'nickname' }">
+           <label for="nickname">닉네임</label>
+           <input type="text" name="nickname" id="nickname" v-model="input.nickname"
+                  @focus="handleInputFocus('nickname')"
+                  @blur="handleInputBlur('nickname')">
+           <span class="spin"></span>
+        </div>
+        <!-- 태그 -->
+        <div class="input" :class="{ 'focused': inputFocused === 'tag', 'blured' : inputBlured === 'tag' }">
+           <label for="nickname">태그</label>
+           <input type="text" name="tag" id="tag" v-model="input.tag"
+                  @focus="handleInputFocus('tag')"
+                  @blur="handleInputBlur('tag')">
+           <span class="spin"></span>
+        </div>
 
-    <div class="box">
-
-      <!-- 닫기 아이콘 -->
-      <button class="close-button" @click="closeForm">
-        <font-awesome-icon :icon="['fas', 'times']" />
-      </button>
-
-      <div class="title">UPLOAD</div>
-
-      <!-- 비디오 아이콘 -->
-      <input type="file" ref="videoInput" style="display: none" @change="fileChange($event)"/>
-      <button class="add-video-button">
-      <font-awesome-icon :icon="['fas', 'video']" @click="openFileInput" style="width:50px; height:50px;"/>
-      <!-- 체크 아이콘 -->
-      <font-awesome-icon :icon="['fas', 'check']" v-if="showCheckIcon" style="color: #28e24d; margin-left: 8px; width:30px; height:30px; display:block;" />
-      </button>
-
-    <!-- Form -->
-    <form method="post" id="videoForm" @submit.prevent="submitForm" enctype="multipart/form-data">
-      <!-- 제목 -->
-      <div class="input" :class="{ 'focused': inputFocused === 'videoTitle', 'blured' : inputBlured === 'videoTitle' }">
-         <label for="videoTitle">Title</label>
-         <input type="text" name="videoTitle" id="videoTitle" v-model="input.title"
-                @focus="handleInputFocus('videoTitle')"
-                @blur="handleInputBlur('videoTitle')">
-         <span class="spin"></span>
-      </div>
-
-      <!-- 내용 -->
-      <div class="input" :class="{ 'focused': inputFocused === 'videoContent', 'blured' : inputBlured === 'videoContent' }">
-         <label for="videoContent">Content</label>
-         <input type="text" name="videoContent" id="videoContent" v-model="input.content"
-                @focus="handleInputFocus('videoContent')"
-                @blur="handleInputBlur('videoContent')">
-         <span class="spin"></span>
-      </div>
-
-      <!-- GO 버튼 -->
-      <div class="button submit">
-         <button type="submit">
-            <span>GO</span> 
-            <i class="fa fa-check"></i>
-         </button>
-      </div>
-
-      <!-- <a href="" class="pass-forgot">Forgot your password?</a> -->
-    </form>
-   </div>
-
-  <!-- 오른쪽 + 버튼인데 안 씀 -->
-   <!-- <div class="overbox">
-      <div class="material-button alt-2"><span class="shape"></span></div>
-
-      <div class="title">REGISTER</div>
-
-      <div class="input">
-         <label for="regname">Username</label>
-         <input type="text" name="regname" id="regname"> 
-         <span class="spin"></span>
-      </div>
-
-      <div class="input">
-         <label for="regpass">Password</label>
-         <input type="password" name="regpass" id="regpass">
-         <span class="spin"></span>
-      </div>
-
-      <div class="input">
-         <label for="reregpass">Repeat Password</label>
-         <input type="password" name="reregpass" id="reregpass">
-         <span class="spin"></span>
-      </div>
-
-      <div class="button">
-         <button><span>NEXT</span></button>
-      </div>
-    </div> -->
-
-</div>
-</template>
-
-
-<script>
-import $ from 'jquery';
-
-export default {
-  name: 'CommunityWrite',
-  data() {
-    return {
-      inputFocused: null,
-      inputBlured: null,
-      showCheckIcon: false,
-      selectedFile: null,
-      input: {
-        title: '',
-        content: '',
-      },
+        <!-- 제목 -->
+        <div class="input" :class="{ 'focused': inputFocused === 'videoTitle', 'blured' : inputBlured === 'videoTitle' }">
+           <label for="videoTitle">Title</label>
+           <input type="text" name="videoTitle" id="videoTitle" v-model="input.title"
+                  @focus="handleInputFocus('videoTitle')"
+                  @blur="handleInputBlur('videoTitle')">
+           <span class="spin"></span>
+        </div>
+  
+        <!-- 내용 -->
+        <div class="input" :class="{ 'focused': inputFocused === 'videoContent', 'blured' : inputBlured === 'videoContent' }">
+          <label for="videoContent">Content</label>
+          <!-- 클릭 시에 textarea가 나타나도록 v-show 디렉티브 사용 -->
+          <textarea v-show="inputFocused === 'videoContent'" v-autosize type="text" name="videoContent" id="videoContent" v-model="input.content"
+                    @blur="handleInputBlur('videoContent')"></textarea>
+          <span class="spin"></span>
+        </div>
+  
+        <!-- GO 버튼 -->
+        <div class="button submit">
+           <button type="submit">
+              <span>GO</span> 
+              <i class="fa fa-check"></i>
+           </button>
+        </div>
+  
+        <!-- <a href="" class="pass-forgot">Forgot your password?</a> -->
+      </form>
+     </div>
+  
+  
+  </div>
+  </template>
+  
+  
+  <script>
+  // v-autosize 디렉티브를 정의합니다.
+const autosize = {
+  bind(el) {
+    el.style.overflow = 'hidden';
+    el.style.resize = 'none';
+    const setStyle = () => {
+      el.style.height = 'auto';
+      el.style.height = `${el.scrollHeight}px`;
     };
-  },
-  components: {
-
-  },
-  mounted() {
-
-  },
-  methods: {
-    createPost() {
-      console.log('Creating post:', this.post);
-    },
-    closeForm() {
-      this.$emit("closeForm");
-    },
-    openFileInput(){
-      // 파일 입력란 클릭 시 파일 다이얼로그 열기
-      return this.$refs.videoInput.click();
-    },
-    handleInputFocus(val) {
-      this.inputFocused = val;
-      this.inputBlured = null;
-    },
-    handleInputBlur(val) {
-      this.inputFocused = null;
-      this.inputBlured = val;
-    },
-    fileChange(event){
-      
-      if (event.target.files.length > 0) {
-        this.selectedFile = event.target.files[0];
-        this.showCheckIcon = true;
-        
-console.log(event.target.files)
-      }
-      
-    },
-    submitForm(event) {
-      const file = this.selectedFile;
-
-      if (file) {
-
-        var formData = new FormData(document.getElementById("videoForm"));
-
-        formData.append('videoFile', file);
-        formData.append('type', file.type);
-        formData.append('inp_dt', formatDate(file.lastModifiedDate ));
-        formData.append('video_no', event._vts);
-
-        // formData 출력
-        for (const [key, value] of formData.entries()) {
-          console.log(`${key}: ${value}`);
-        }
-
-          $.ajax({
-          type: "POST",
-          url: "http://localhost:7001/api/upload",
-          data: formData,
-          processData: false,
-          contentType: false,
-          success: function() {
-            alert("업로드맨");
-            this.$router.go(0); // 현재 페이지 리랜더링
-          }.bind(this),
-          error: function(xhr, status, error) {
-            alert("에러 발생: " + error);
-          }
-        });
-        
-      }
-    },
-
-
+    el.addEventListener('input', setStyle);
+    setStyle();
   },
 };
 
+  export default {
+    name: 'CommunityWrite',
+    directives: {
+    autosize,
+  },
+    data() {
+      return {
+        inputFocused: null,
+        inputBlured: null,
+        showCheckIcon: false,
+        selectedFile: null,
+        input: {
+          nickname: '',  
+          tag: '',
+          title: '',
+        },
+      };
+    },
+    components: {
+  
+    },
+    mounted() {
+  
+    },
+    methods: {
+      createPost() {
+        console.log('Creating post:', this.post);
+      },
+      closeForm() {
+        this.$emit("closeForm");
+      },
+      handleInputFocus(val) {
+        this.inputFocused = val;
+        this.inputBlured = null;
+      },
+      handleInputBlur(val) {
+      this.inputFocused = null;
+      this.inputBlured = val;
 
-function formatDate(date) {
-  const year = String(date.getFullYear()).slice(-2);
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
+      // title 값 있을 때
+      if(this.input.nickname !== ''){
+        this.input.nicknameStyle  = {
+          'line-height': '18px',
+          'font-size': '1px',
+        };
+      }else{
+        this.input.nicknameStyle = {};
+      }
 
-  return `${year}${month}${day}`;
-}
-
-
-</script>
-
-
-<style scoped>
-
-@import '@/css/addVideoForm.css';
-
-.focused label {
-  line-height: 18px;
-  font-size: 18px;
-  font-weight: 100;
-  top: 0px;
-}
-
-.focused .spin {
-  width: 100%;
-}
-
-.blured label {
-  line-height: 60px;
-  font-size: 24px;
-  font-weight: 300;
-  top: 10px;
-}
-
-.blured .spin {
-  width: 0%;
-}
-
-.materialContainer {
+      // content 값 있을 때
+      if(this.input.content !== ''){
+        this.input.contentStyle = {
+          'line-height': '18px',
+          'font-size': '10px',
+        };
+      }else{
+        this.input.contentStyle = {};
+      }
+    },
+  
+  
+    },
+  };
+  
+  
+  </script>
+  
+  
+  <style scoped>
+  
+  /* @import '@/css/addVideoForm.css'; */
+  
+  .focused label {
+    line-height: 5px;
+    font-size: 15px;
+    font-weight: 100;
+    top: 0px;
+  }
+  
+  .focused .spin {
+    width: 80%;
+  }
+  
+  .blured label {
+    line-height: 50px;
+    font-size: 20px;
+    font-weight: 300;
+    top: 0px;
+  }
+  
+  .blured .spin {
+    width: 0%;
+  }
+  
+  .materialContainer {
     width: 100%;
     max-width: 1365px;
     position: absolute;
     top: 49%;
     left: 52%;
-    /* right: 30%; */
     transform: translate(-50%, -50%);
     -webkit-transform: translate(-50%, -50%);
     -ms-transform: translate(-50%, -50%);
-    /* height: 849px; */
-}
-</style>
+  }
+  
+  .ui {
+    z-index: 1; /* 화면위에 나타남 */
+  }
+  
+  .put_inquiry {
+      width: 204px;
+      height: 33px;
+      padding: 0 12px;
+      line-height: 35px;
+      border: 1px solid #cecdce;
+      color: #333;
+  }
+
+  .title{
+    text-align: center;
+  }
+  </style>
--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>
-        <section class="content-feature" style="" data-v-476b23f8="" data-v-676cec7c=""><div class="inner_path" data-v-476b23f8=""><h3 class="screen_out" data-v-476b23f8=""></h3> 
-            <div class="wrap_tit" data-v-476b23f8="">
-                <img data-v-a968263a="" data-v-476b23f8="" src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/657726a1017800001.png?type=thumb&amp;opt=C72x72.fwebp" alt=""> 
-                <strong class="tit_path" data-v-476b23f8="" style="font-weight:bold; font-size:xx-large">&nbsp; 커뮤니티</strong></div> <!----> <!---->
-            </div>
-        </section>
-        <ul data-v-73310296="" role="tablist" aria-labelledby="titNews" class="list_tab">
+  <div>
+    <section class="content-feature" style="" data-v-476b23f8="" data-v-676cec7c="">
+      <div class="inner_path" data-v-476b23f8="">
+        <h3 class="screen_out" data-v-476b23f8=""></h3>
+        <div class="wrap_tit" data-v-476b23f8="">
+          <img
+            data-v-a968263a=""
+            data-v-476b23f8=""
+            src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/657726a1017800001.png?type=thumb&amp;opt=C72x72.fwebp"
+            alt=""
+          />
+          <strong class="tit_path" data-v-476b23f8="" style="font-weight: bold; font-size: xx-large">
+            &nbsp; 커뮤니티
+          </strong>
+        </div>
+      </div>
+    </section>
+    <ul data-v-73310296="" role="tablist" aria-labelledby="titNews" class="list_tab">
       <li data-v-73310296="" role="presentation">
         <button
           type="button"
@@ -52,74 +62,120 @@
           :class="{ 'on': selectedTab === 'mediaRelations' }"
           @click="selectedTab = 'mediaRelations'"
         >
-        &nbsp;공지&nbsp;
+          &nbsp;공지&nbsp;
         </button>
       </li>
       <li data-v-73310296="" role="presentation" style="margin-top: 36px;">
-          <div class="switch_btnbox">
-                <!-- &nbsp;<a class="btn_write txt" href="">글쓰기</a> -->
-                &nbsp;<a class="btn_write txt" @click="showWritePage = true">글쓰기</a>
-                <!-- 글쓰기 화면 표시 -->
-                <community-write v-if="showWritePage" />
-          </div>
-
+        <div class="switch_btnbox">
+          <!-- &nbsp;<a class="btn_write txt" href="">글쓰기</a> -->
+          <!-- &nbsp;<a class="btn_write txt" @click="showWritePage = true" style="width:85px; height:36px; margin-top:-15px; ">글쓰기</a> -->
+          <a class="btn_write txt" @click="openWriteForm" style="width:85px; height:36px; margin-top:-15px; ">글쓰기</a>
+          <!-- 글쓰기 화면 표시 -->
+          <!-- <community-write v-if="showWritePage" /> -->
+          <!-- 모달을 열 때 CommunityWrite 컴포넌트 사용 -->
+          <CommunityWrite v-if="showWritePage" @closeForm="closeWriteForm" />
+        </div>
       </li>
     </ul>
-    </div>
-    <ul id="tabNewsContent1" role="tabpanel" aria-labelledby="tabNews1" class="list_card list_card_type2" data-v-676cec7c="">
-        <li data-v-676cec7c="">
-            <div data-v-676cec7c="" class="item_card" style="background-color:white">
-                <span class="wrap_cont"><span role="text" class="info_cate"  style="margin-left: 5px;">
-                    <img data-v-a968263a="" src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/6562f7bc017800001.png?type=thumb&amp;opt=C72x72.fwebp" width="36" height="36" alt="" class="ico_cate">
-                    <span class="txt_date">
-                        <span class="screen_out" style="font-weight: bold;">
-                            찰장군b&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        </span>
-                            2023.11.17 17:50&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <img data-v-a968263a="" src="https://img.freepik.com/free-photo/3d-render-thumb-up-sign-isolated-hand-gesture_107791-15906.jpg?size=626&amp;ext=jpg&amp;ga=GA1.1.43073888.1700201348&amp;semt=sph" width="30" height="30" alt="" class="ico_cate">
-                        10
-      </span> <!----></span> 
-      <a href="/page/detail/10712" class="link_item" id="card10712" data-tiara-layer="click_all_1" data-tiara-action-name="click_all_1">
-        <strong class="tit_card">웹투게더 어린이집 근로복지공단 보육공모전 최우수나이스데</strong> <span class="wrap_thumb">
-            </span></a> <span role="text" class="info_card">
-                <span class="txt_keyword">#남구로</span>
-                <span class="txt_keyword">#구로디지털단지</span>
-                <span class="txt_keyword">#맛집없음</span>
-            </span></span> 
-            </div>
-        </li>
-    </ul>
-
-  </template>
+  </div>
+  <ul id="tabNewsContent1" role="tabpanel" aria-labelledby="tabNews1" class="list_card list_card_type2" data-v-676cec7c="">
+    <li data-v-676cec7c="">
+      <div data-v-676cec7c="" class="item_card" style="background-color: white">
+        <span class="wrap_cont">
+          <span role="text" class="info_cate" style="margin-left: 5px;">
+            <img
+              data-v-a968263a=""
+              src="https://t1.kakaocdn.net/kakaocorp/kakaocorp/admin/6562f7bc017800001.png?type=thumb&amp;opt=C72x72.fwebp"
+              width="36"
+              height="36"
+              alt=""
+              class="ico_cate"
+            />
+            <span class="txt_date">
+              <span class="screen_out" style="font-weight: bold;">
+                찰장군b&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </span>
+              2023.11.17 17:50&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <img
+                data-v-a968263a=""
+                src="https://img.freepik.com/free-photo/3d-render-thumb-up-sign-isolated-hand-gesture_107791-15906.jpg?size=626&amp;ext=jpg&amp;ga=GA1.1.43073888.1700201348&amp;semt=sph"
+                width="30"
+                height="30"
+                alt=""
+                class="ico_cate"
+              />
+              10
+            </span>
+          </span>
+          <a href="/page/detail/10712" class="link_item" id="card10712" data-tiara-layer="click_all_1" data-tiara-action-name="click_all_1">
+            <strong class="tit_card">웹투게더 어린이집 근로복지공단 보육공모전 최우수나이스데</strong>
+            <span class="wrap_thumb"></span>
+          </a>
+          <span role="text" class="info_card">
+            <span class="txt_keyword">#남구로</span>
+            <span class="txt_keyword">#구로디지털단지</span>
+            <span class="txt_keyword">#맛집없음</span>
+          </span>
+        </span>
+      </div>
+    </li>
+  </ul>
   
-  <script>
+</template>
+
+<script>
 import CommunityWrite from '@/components/CommunityWrite.vue';
+
 export default {
-    components: {
-      CommunityWrite,
-    },
-    data() {
+  components: {
+    CommunityWrite,
+  },
+  data() {
     return {
       showWritePage: false,
     };
   },
-    mounted() {
-        // 컴포넌트가 마운트될 때 실행되는 로직
-        this.removeNavbarElement();
+  mounted() {
+    // 컴포넌트가 마운트될 때 실행되는 로직
+    this.toggleNavbarElement(false); // .navbar 엘리먼트를 숨김
+  },
+  beforeUnmount() {
+    // 컴포넌트가 언마운트되기 전에 실행되는 로직
+    this.toggleNavbarElement(true); // .navbar 엘리먼트를 표시
+  },
+  methods: {
+    toggleNavbarElement(show) {
+      // .navbar 클래스를 가진 엘리먼트를 찾아서 표시 또는 숨김
+      const navbarElement = document.querySelector('.navbar');
+      if (navbarElement) {
+        navbarElement.style.display = show ? 'block' : 'none';
+      }
     },
-    methods: {
-        removeNavbarElement() {
-            // 클래스가 navbar인 엘리먼트를 찾아 제거
-            const navbarElement = document.querySelector('.navbar');
-            if (navbarElement) {
-                navbarElement.remove();
-            }
-        }
-    }
+    openWriteForm() {
+      // 모달을 열 때 필요한 로직 작성...
+      this.showWritePage = true;
+    },
+    closeWriteForm() {
+      // 모달을 닫을 때 필요한 로직 작성...
+      this.showWritePage = false;
+    },
+  },
 };
-  </script>
-  
-  <style scoped>
-    @import '@/css/communityView.css';
-  </style>
-  
+</script>
+
+
+
+<style scoped>
+  @import '@/css/communityView.css';
+
+  /* 2. '글쓰기' 버튼을 둥근 코너로 만들기 */
+  .btn_write {
+    border-radius: 20px;
+    display: inline-block; /* 둥근 코너를 적용하기 위해 inline-block으로 표시 */
+  }
+
+  /* 3. '글쓰기' 버튼에 호버 효과 추가하기 */
+  .btn_write:hover {
+    cursor: pointer;
+  }
+</style>
