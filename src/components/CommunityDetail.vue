893e274fa68e73f5930c1a11f2c7cdc7c4cb30b1
Limsukhyun <as17269@naver.com>
2023-11-27 18:07:50 +0900

    비상!!비상!!

--- src/components/CommunityDetail.vue
+++ src/components/CommunityDetail.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>ho</div>
+  <div>
+    <!-- CommunityDetail 컴포넌트 내용은 생략하고 필요한 경우에 채워넣으세요. -->
+    <div>ho</div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: ['showDetailPage'], // showDetailPage를 prop으로 받아옵니다.
+
+  // 컴포넌트 로직 등을 추가하세요.
+};
+</script>
